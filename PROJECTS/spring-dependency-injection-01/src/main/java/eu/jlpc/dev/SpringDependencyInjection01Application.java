25b008e253dc99c9e833aadebdb35979ff6e4bd6
HippieStereo <stereobroo@yahoo.com>
2021-03-17 12:26:33 +0000

    Create Project
    NAME : spring-dependency-injection-01

---
+++ PROJECTS/spring-dependency-injection-01/src/main/java/eu/jlpc/dev/SpringDependencyInjection01Application.java
@@ -0,0 +1,13 @@
+package eu.jlpc.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDependencyInjection01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDependencyInjection01Application.class, args);
+	}
+
+}
---
+++ PROJECTS/spring-dependency-injection-01/src/test/java/eu/jlpc/dev/SpringDependencyInjection01ApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.jlpc.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDependencyInjection01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
